fd6a3feeb4aa5efc7235911c50bc8991103a5764
Juan J. Rodríguez León <juan.rodriguez82@upr.edu>
2022-01-01 18:30:36 -0400

    Finished all Chapter 2 examples and finishing up Chapter 3 Note taking

--- src/TestScript.py
+++ src/TestScript.py
@@ -1,2 +1,12 @@
 print('Hello World');
 
+print('P' in "Phyton")
+#print('p' in "Phyton")
+
+
+website = 'http://www.python.org'
+#website[-3:] = 'com' 
+
+format = "Hello, %s. %s enough for ya?"
+values = ('Earth', 'Hot')
+print(format % values)
